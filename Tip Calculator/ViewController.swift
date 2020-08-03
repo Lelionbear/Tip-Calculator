1e103f7cb28557aa7bc34b8782de5159a3664b2f
Arturo Galvan <agalvanalarcon@gmail.com>
2020-08-02 22:59:41 -0700

    stashing?

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    
+    
+    @IBOutlet weak var tipLable: UILabel!
+    
+    
+    @IBOutlet weak var totalLable: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipArr = [0.15,0.18,0.2]
+        
+        let tip = bill * tipArr[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLable.text = String(format: "$%.2f", tip)
+        totalLable.text = String(format: "$%.2f", total)
+    }
 }
 
